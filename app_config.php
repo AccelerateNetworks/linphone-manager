2aefe4c248e86e6ea5d19e691f623d38f8ef0268
Jesse Gruver (piajesse) <me@piajesse.com>
2022-05-30 18:57:38 -0700

    setup

---
+++ app_config.php
@@ -0,0 +1,19 @@
+<?php
+	//application details
+		$apps[$x]['name'] = "Linphone-Manager";
+		$apps[$x]['uuid'] = "30f48572-0250-4b58-8ac7-df0a77fc4bc3";
+		$apps[$x]['category'] = "App";
+		$apps[$x]['subcategory'] = "";
+		$apps[$x]['version'] = "0.1";
+		$apps[$x]['license'] = "GNU General Public License v3";
+		$apps[$x]['url'] = "https://github.com/acceleratenetworks/linphone-manager";
+		$apps[$x]['description']['en-us'] = "FusionPBX Linphone Manager";
+		$apps[$x]['description']['es-cl'] = "";
+		$apps[$x]['description']['de-de'] = "";
+		$apps[$x]['description']['de-ch'] = "";
+		$apps[$x]['description']['de-at'] = "";
+		$apps[$x]['description']['fr-fr'] = "";
+		$apps[$x]['description']['fr-ca'] = "";
+		$apps[$x]['description']['fr-ch'] = "";
+		$apps[$x]['description']['pt-pt'] = "";
+		$apps[$x]['description']['pt-br'] = "";
